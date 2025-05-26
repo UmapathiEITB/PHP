eb8010db1f78fad77d0ab6cb94832fb97dcfdfc5
Umapathi <umapathi@elephantintheboardroom.com.au>
2025-05-26 17:56:23 +0530

    Inheritance

---
+++ oops/inheritance.php
@@ -0,0 +1,15 @@
+<?php
+class V{
+    public function m() {
+        /** you logic */
+    }
+}
+class B extends V{
+    public function a() {
+        /** your logic */
+    }
+}
+
+$b = new B();
+$b->m();     
+$b->a(); 
